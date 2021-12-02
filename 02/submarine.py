93669be65db6a9220166abefe211585808585bdc
Guillem <guillemglez@users.noreply.github.com>
2021-12-02 22:05:53 +0100

    Day 2, part 2

--- 02/submarine.py
+++ 02/submarine.py
@@ -1,6 +1,8 @@
 def submarine(input_file: str) -> None:
     x: int = 0
-    d: int = 0
+    d_one: int = 0
+    d_two: int = 0
+    a: int = 0
 
     with open(input_file) as f:
         for line in f:
@@ -9,12 +11,18 @@ def submarine(input_file: str) -> None:
 
             if instruction == "forward":
                 x += units
+                d_two += a * units
             if instruction == "down":
-                d += units
+                d_one += units
+                a += units
             if instruction == "up":
-                d -= units
+                d_one -= units
+                a -= units
 
-    print(f"Multiplying final horizontal position by final depth gives {x*d}")
+    print(
+        f"Multiplying final horizontal position by final depth gives {x*d_one}"
+    )
+    print(f"Using the new interpretation, it gives {x*d_two}")
 
 
 if __name__ == "__main__":
