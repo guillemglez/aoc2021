5d8e0c8ed704ffce68dd012d481e795fcd81b54d
Guillem <guillemglez@users.noreply.github.com>
2021-12-06 20:58:34 +0100

    Day 5, part 2

--- 05/vents.py
+++ 05/vents.py
@@ -12,7 +12,7 @@ class Point:
         return f"{self.x},{self.y}"
 
 
-def parse(input: str) -> List[Tuple[Point, Point]]:
+def vents(input: str) -> None:
     coords: List[Tuple[Point, Point]] = []
     with open(input) as f:
         for line in f:
@@ -22,32 +22,39 @@ def parse(input: str) -> List[Tuple[Point, Point]]:
             first = Point(first_raw[0], first_raw[1])
             second = Point(second_raw[0], second_raw[1])
             coords.append((first, second))
-    return coords
 
-
-def one(coords: List[Tuple[Point, Point]]) -> int:
     xmax, ymax = 0, 0
     for coord in coords:
         xmax = max(xmax, coord[0].x, coord[1].x)
         ymax = max(ymax, coord[0].y, coord[1].y)
 
-    diagram = np.zeros((ymax + 1, xmax + 1), dtype=int)
-    for first, second in coords:
-        # Only vertical or horizontal
-        if not ((first.x == second.x) or (first.y == second.y)):
-            continue
+    diagram_one = np.zeros((ymax + 1, xmax + 1), dtype=int)
+    diagram_two = diagram_one.copy()
 
+    for first, second in coords:
         x1 = min(first.x, second.x)
         x2 = max(first.x, second.x) + 1
         y1 = min(first.y, second.y)
         y2 = max(first.y, second.y) + 1
-        diagram[y1:y2, x1:x2] += 1
 
-    return np.count_nonzero(diagram > 1)
+        ortogonal = first.x == second.x or first.y == second.y
+        if (ortogonal):
+            diagram_one[y1:y2, x1:x2] += 1
+            diagram_two[y1:y2, x1:x2] += 1
+            continue
 
+        # Diagonal
+        eye = np.eye(y2 - y1, dtype=int)
+        inverted = (first.x - second.x) * (first.y - second.y) < 0
+        if (inverted):
+            eye = eye[::-1, ...]
 
-def vents(input: str) -> None:
-    print(f"Lines overlap at {one(parse(input))} points")
+        diagram_two[y1:y2, x1:x2] += eye
+
+    print(f"Lines overlap at {np.count_nonzero(diagram_one > 1)} points")
+    print(
+        f"When taking diagonals into account, lines overlap at {np.count_nonzero(diagram_two > 1)} points"
+    )
 
 
 if __name__ == "__main__":
