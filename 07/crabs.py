81105163ceecc0263ba9de1afb3e0ddc736b1a8b
Guillem <guillemglez@users.noreply.github.com>
2021-12-08 18:23:39 +0100

    Day 7, part 2

--- 07/crabs.py
+++ 07/crabs.py
@@ -1,14 +1,20 @@
-from statistics import median
+from statistics import median, mean
 
 
 def crabs(input: str) -> None:
     with open(input) as f:
         crabs = [int(x) for x in f.readline().strip().split(',')]
 
+    # Part 1
     target_pos = int(median(crabs))
     fuel = sum([abs(crab - target_pos) for crab in crabs])
-
     print(f"Aligning the crabs would cost {fuel} fuel")
+
+    # Part 2
+    target_pos = round(mean(crabs))
+    nth = lambda n: int(n * (n + 1) / 2)  # Sum of N natural numbers
+    fuel = sum([nth(abs(crab - target_pos)) for crab in crabs])
+    print(f"Having understood crab engineering, it would cost {fuel} fuel")
 
 
 if __name__ == "__main__":
