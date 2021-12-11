648d83a89321eeca28c596ea4be4ae030b1c8400
Guillem <guillemglez@users.noreply.github.com>
2021-12-11 01:57:56 +0100

    Day 9, part 2

--- 09/smoke.py
+++ 09/smoke.py
@@ -1,5 +1,19 @@
-from typing import List
+from typing import List, Tuple
 import numpy as np
+
+
+def get_neighbors(index: Tuple[int, int],
+                  shape: Tuple[int, ...]) -> List[Tuple[int, int]]:
+    neighbors: List[Tuple[int, int]] = []
+    r, c = index
+    sr, sc = shape
+    for i in (-1, 1):
+        if (r + i) in range(sr):
+            neighbors.append((r + i, c))
+    for j in (-1, 1):
+        if (c + j) in range(sc):
+            neighbors.append((r, c + j))
+    return neighbors
 
 
 def smoke(input: str) -> None:
@@ -10,25 +24,40 @@ def smoke(input: str) -> None:
 
     heightmap = np.array(matrix)
     risk = 0
+    basins: List[int] = []
     with np.nditer(heightmap, flags=['multi_index'],
                    op_flags=['readonly']) as it:
         for height in it:
-            neighbors: List[int] = []
+            neighbors = get_neighbors(it.multi_index, heightmap.shape)
 
-            tryAppend = lambda r, c: neighbors.append(heightmap[r, c]) if (
-                (r in range(heightmap.shape[0])) and
-                (c in range(heightmap.shape[1]))) else None
+            lowvalues: List[int] = []
+            for neighbor in neighbors:
+                lowvalues.append(heightmap[neighbor])
 
-            r, c = it.multi_index
-            for i in (-1, 1):
-                tryAppend(r + i, c)
-            for j in (-1, 1):
-                tryAppend(r, c + j)
-
-            if all([neighbor > height for neighbor in neighbors]):
+            # If a low point
+            if all([neighbor > height for neighbor in lowvalues]):
+                # Part 1
                 risk += height + 1
 
+                # Part 2
+                basin = [it.multi_index]
+                previous = -1
+                while previous != len(basin):
+                    previous = len(basin)
+                    for index in basin:
+                        for neighbor in get_neighbors(index, heightmap.shape):
+                            if neighbor not in basin and heightmap[
+                                    neighbor] != 9:
+                                basin.append(neighbor)
+                basins.append(len(basin))
+
+    # Calculate part 2
+    sizemult = 1
+    for i in range(3):
+        sizemult *= basins.pop(basins.index(max(basins)))
+
     print(f"The sum of the risk levels is {risk}")
+    print(f"Multiplying the three biggest basins gives {sizemult}")
 
 
 if __name__ == "__main__":
